4cd82acb34a7d9eafae3e3401fc3276345aff465
fakhrulfouad <66157555+fakhrulfouad@users.noreply.github.com>
2020-09-01 10:40:35 +0800

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Muhammad Fakhrulghazi bin Mohd Fouad on 18/08/2020.
+//  Copyright © 2020 Muhammad Fakhrulghazi bin Mohd Fouad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
